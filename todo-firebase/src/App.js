3ac194f2a8d8578b1a80d166db167fad3adc33ad
Deep Kiran <cdeepkiran14@gmail.com>
2021-04-11 03:06:40 +0530

    Add tasks to Firestore db collection

--- todo-firebase/src/App.js
+++ todo-firebase/src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import React,{useState,useEffect} from "react";
 import Todo from "./components/Todo";
 import {db} from "./firebase";
-
+import firebase from "firebase";
 function App() {
   const [todos,setTodos]=useState([]);
   const [input,setInput]=useState('');
 
 useEffect(()=>{
-  db.collection('todos').onSnapshot(snapshot=>{setTodos(snapshot.docs.map(doc=>doc.data().todo))})
+  db.collection('todos').orderBy('timestamp','desc').onSnapshot(snapshot=>{setTodos(snapshot.docs.map(doc=>doc.data().todo))})
 },[]);
 
   const addTask=(e)=>{
     e.preventDefault();
-    setTodos(prevtodos=>[...prevtodos,input]);
+    db.collection('todos').add({'todo':input,'timestamp':firebase.firestore.FieldValue.serverTimestamp()})
     setInput('');
   }
   return (
