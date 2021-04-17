6c70043becc19e3f3493f1cb2bc5766bf6a64db0
Deep Kiran <cdeepkiran14@gmail.com>
2021-04-17 13:52:55 +0530

    Refactor Code - Edit Functionality

--- todo-firebase/src/components/Todo.js
+++ todo-firebase/src/components/Todo.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import {db} from "../firebase"; 
 
 import "../styles/todo.css";
-function Todo({id,todo}) {
+function Todo({todo,id}) {
 const [input,setInput]=useState('');
     
     const deleteTodo=()=>{
@@ -10,42 +10,49 @@ const [input,setInput]=useState('');
         db.collection("todos").doc(id).delete();
     }
 
-    function editTodo(todoid){
+    function editTodo(){
         
-        db.collection("todos").doc(todoid).update({todo:input});
+        db.collection("todos").doc(id).update({todo:input});
         setInput("");
         
-        var modal=document.getElementById("myModal");
+        var modal=document.getElementById(id);
         modal.style.display="none";
      
     }
-    function openbox(id)
+    function openbox()
     {
         console.log("ID",id);
    
    
-        var modal=document.getElementById("myModal");
+        var modal=document.getElementById(id);
         modal.style.display="block";
         
+  
         var span = document.getElementsByClassName("close")[0];
-        span.onclick=function(){modal.style.display="none"};
     }
+
+    const handleSpanClick=()=>{
+        var modal=document.getElementById(id);
+        modal.style.display="none";
+    }
+   
     return (
         <div key={id}>
             <ul style={{listStyle:'none'}}>
                 <div className="todo-item">
                     <li>{todo}</li>
-                    <span style={{marginLeft:"0.5rem"}} id={id}  onClick={openbox}>Edit</span>
+                    <span style={{marginLeft:"0.5rem"}}   onClick={openbox}>Edit</span>
                     <span style={{marginLeft:"0.5rem"}}  onClick={deleteTodo}>&times;</span>
                    
                 </div>
             </ul>
-            <div id="myModal" class="modal">
-                        <span class="close">&times;</span>
-                        <div className="inputSection">
-                            <input className="inputTask" type="text" value={input} onChange={e=>setInput(e.target.value)}/>
-                            <button disabled={!input}  onClick={editTodo}>Edit Task</button>
-                        </div> 
+            <div id={id} class="modal">
+                        <span onClick={handleSpanClick} class="close">&times;</span>
+                            <div className="inputSection">
+                                <input className="inputTask" type="text" value={input} onChange={e=>setInput(e.target.value)}/>
+                                <button disabled={!input}  onClick={editTodo}>Edit Task</button>
+                            </div>
+                        
             </div>
         </div>
     )
